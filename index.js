36f75ae8537c83ebd60805fc10efa3e3e55a37fe
Rayhan Islam <merayhanislam21@gmail.com>
2026-02-05 21:44:56 +0600

    update delete done

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const prisma = new PrismaClient();
 
 const main = async () => {
 
-    const insertUser = await prisma.user.createMany({
-        data: [
-            { name: "Arafat", email: "arafat2@gmail.com" },
-            { name: "Sami", email: "sami3@gmail.com" },
-            { name: "Nabil", email: "nabil4@gmail.com" },
-            { name: "Hasan", email: "hasan5@gmail.com" }
-        ]
-    })
+    // const insertUser = await prisma.user.createMany({
+    //     data: [
+    //         { name: "Arafat", email: "arafat2@gmail.com" },
+    //         { name: "Sami", email: "sami3@gmail.com" },
+    //         { name: "Nabil", email: "nabil4@gmail.com" },
+    //         { name: "Hasan", email: "hasan5@gmail.com" }
+    //     ]
+    // })
 
-    console.log(insertUser);
+    // const deletUser = await prisma.user.delete({
+    //     where:{
+    //         email:"arafat2@gmail.com"
+    //     }
+    // })
+
+    const updateUser = await prisma.user.update({
+        where:{
+            email:"sami3@gmail.com"
+        },
+        data:{
+            name:"Samiul Islam"
+        }
+    })
 
     const users = await prisma.user.findMany();
     console.log(users);
